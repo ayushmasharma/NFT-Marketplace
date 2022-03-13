ff1a8e457bfd55d4ee0c77ff220b0d2db4c5e5e5
Vaibhavi Thakare <84630655+alwaystanaya20@users.noreply.github.com>
2022-03-13 08:40:20 +0530

    Update hardhat.config.js
    Chose Mainnet network to deploy the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "129f803899a44eb0ab40f0dc09c1463f"
+
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -7,10 +11,12 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [process.env.privateKey]
-    // }
+    
+    mainnet: {
+      url: 'https://mainnet.infura.io/v3/${projectId}',
+      accounts:[privateKey]
+    }
+    
   },
   solidity: {
     version: "0.8.4",
